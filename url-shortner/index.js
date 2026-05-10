9c7a5f9b85fc374b970d867bfc58b170f4d68010
ayesha2104 <ayeshamohapatra12@gmail.com>
2026-05-10 10:29:34 +0530

    updated at fix

--- url-shortner/index.js
+++ url-shortner/index.js
@@ -16,6 +16,7 @@ app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
 }));
+app.set('trust proxy', 1);
 
 app.use(express.json());
 app.use(globalLimiter);
