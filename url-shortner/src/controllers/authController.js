40b16a6dfe3e7e176cef98728a037c2d61bf0a11
ayesha2104 <ayeshamohapatra12@gmail.com>
2026-05-10 10:09:14 +0530

    debug expires In fallback 2

--- url-shortner/src/controllers/authController.js
+++ url-shortner/src/controllers/authController.js
@@ -84,7 +84,7 @@ exports.login = async (req, res) => {
         const token = jwt.sign(
             { id: user.id, email: user.email },
             process.env.JWT_SECRET,
-            { expiresIn: process.env.JWT_EXPIRY }
+            { expiresIn: process.env.JWT_EXPIRY || '7d' }
         );
 
         res.json({
